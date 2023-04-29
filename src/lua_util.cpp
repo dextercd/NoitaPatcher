8a408a63cdef698519960162193cfe5f3bf3cda5
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-04-29 14:01:35 +0200

    Lua stack trace utilities

--- src/lua_util.cpp
+++ src/lua_util.cpp
@@ -1,9 +1,45 @@
 #include "lua_util.hpp"
 
+#include <utility>
+
 lua_CFunction get_lua_c_binding(lua_State* L, const char* func_name)
 {
     lua_getglobal(L, func_name);
     auto f = lua_tocfunction (L, -1);
     lua_pop(L, 1);
     return f;
+}
+
+std::optional<stack_entry> get_stack_entry(lua_State* L, int level)
+{
+    lua_Debug debug{};
+    if (!lua_getstack(L, level, &debug))
+        return std::nullopt;
+
+    if (!lua_getinfo(L, "Snl", &debug))
+        return std::nullopt;
+
+    return stack_entry{
+        .source = debug.source,
+        .function_name = (debug.name ? debug.name : "<unknown func>"),
+        .line_number = debug.currentline,
+    };
+}
+
+std::vector<stack_entry> get_stack_trace(lua_State* L, int start_level, int count)
+{
+    std::vector<stack_entry> trace;
+
+    for (int level = start_level; true; ++level) {
+        if (count != -1 && count-- != 0)
+            break;
+
+        auto stack = get_stack_entry(L, level);
+        if (!stack)
+            break;
+
+        trace.push_back(*stack);
+    }
+
+    return trace;
 }
--- src/lua_util.hpp
+++ src/lua_util.hpp
@@ -5,6 +5,19 @@ extern "C" {
 #include <lua.h>
 }
 
+#include <optional>
+#include <string>
+#include <vector>
+
+struct stack_entry {
+    std::string source;
+    std::string function_name;
+    int line_number;
+};
+
 lua_CFunction get_lua_c_binding(lua_State* L, const char* func_name);
+
+std::optional<stack_entry> get_stack_entry(lua_State* L, int level);
+std::vector<stack_entry> get_stack_trace(lua_State* L, int start_level, int count = -1);
 
 #endif // Header guard
