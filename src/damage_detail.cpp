336dd34a38745cbc6a8369d483a293b1febf4a54
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-05-19 21:31:16 +0200

    Use lua_rawseti instead

--- src/damage_detail.cpp
+++ src/damage_detail.cpp
@@ -91,21 +91,17 @@ int GetDamageDetails(lua_State* L)
     lua_setfield(L, -2, "ragdoll_fx");
 
     lua_createtable(L, 2, 0);
-    lua_pushinteger(L, 1);
     lua_pushnumber(L, details.impulse.x);
-    lua_settable(L, -3);
-    lua_pushinteger(L, 2);
+    lua_rawseti(L, -2, 1);
     lua_pushnumber(L, details.impulse.y);
-    lua_settable(L, -3);
+    lua_rawseti(L, -2, 2);
     lua_setfield(L, -2, "impulse");
 
     lua_createtable(L, 2, 0);
-    lua_pushinteger(L, 1);
     lua_pushnumber(L, details.world_pos.x);
-    lua_settable(L, -3);
-    lua_pushinteger(L, 2);
+    lua_rawseti(L, -2, 1);
     lua_pushnumber(L, details.world_pos.y);
-    lua_settable(L, -3);
+    lua_rawseti(L, -2, 2);
     lua_setfield(L, -2, "world_pos");
 
     lua_pushinteger(L, details.knockback_force);
