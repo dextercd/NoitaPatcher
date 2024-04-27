118397a41254503db7af337ea6da5568304781b9
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2024-04-27 14:41:25 +0200

    Allow arguments and return values

--- src/crosscall.cpp
+++ src/crosscall.cpp
@@ -40,48 +40,77 @@ int CrossCallAdd(lua_State* L)
     return 0;
 }
 
+void copy_value(lua_State* to, lua_State* from, int from_index)
+{
+    auto type = lua_type(from, from_index);
+    if (type == LUA_TBOOLEAN) {
+        lua_pushboolean(to, lua_toboolean(from, from_index));
+    } else if (type == LUA_TNUMBER) {
+        lua_pushnumber(to, lua_tonumber(from, from_index));
+    } else if (type == LUA_TSTRING) {
+        std::size_t length;
+        auto str = lua_tolstring(from, from_index, &length);
+        lua_pushlstring(to, str, length);
+    } else if (type == LUA_TLIGHTUSERDATA) {
+        lua_pushlightuserdata(to, lua_touserdata(from, from_index));
+    } else { // Nil or unsupported type
+        lua_pushnil(to);
+    }
+}
+
+const int MAX_CC_RETURN = 10;
+
 int CrossCall(lua_State* L)
 {
     if (!current_lua_state)
         return luaL_error(L, "No current Lua state.");
 
-    if (lua_gettop(L) != 1 || !lua_isstring(L, 1))
-        return luaL_error(L, "Expected single function name argument.");
+    int restore_top = lua_gettop(current_lua_state);
 
-    auto f = registered_funcs.find(lua_tostring(L, 1));
-    if (f == std::end(registered_funcs))
+    int l_top = lua_gettop(L);
+    int argument_count = l_top - 1;
+
+    if (l_top < 1 || !lua_isstring(L, 1))
+        return luaL_error(L, "Expected a function name argument.");
+
+    auto reg_iterator = registered_funcs.find(lua_tostring(L, 1));
+    if (reg_iterator == std::end(registered_funcs))
         return luaL_error(L, "Requested function does not exist.");
 
-    auto ref = f->second;
-
-    auto current_top = lua_gettop(current_lua_state);
+    // Push the requested function
+    int ref = reg_iterator->second;
     lua_rawgeti(current_lua_state, LUA_REGISTRYINDEX, ref);
-    auto call_result = lua_pcall(current_lua_state, 0, 1, 0);
+
+    // Special case, not actually a cross call
+    if (L == current_lua_state) {
+        lua_insert(current_lua_state, 1);
+        lua_call(current_lua_state, argument_count, MAX_CC_RETURN);
+        return MAX_CC_RETURN;
+    }
+
+    // Copy arguments across Lua states
+    lua_checkstack(current_lua_state, lua_gettop(current_lua_state) + argument_count);
+    for (int i = 2; i <= l_top; ++i) {
+        copy_value(current_lua_state, L, i);
+    }
+
+    int call_result = lua_pcall(current_lua_state, argument_count, MAX_CC_RETURN, 0);
 
     if (call_result != 0) {
         auto err = std::string{lua_tostring(current_lua_state, -1)};
-        lua_settop(current_lua_state, current_top);
-        return luaL_error(L, lua_tostring(current_lua_state, 1));
+        lua_settop(current_lua_state, restore_top);
+        return luaL_error(L, err.c_str());
     }
 
-    auto has_return_value = false;
-    std::string return_value;
-
-    if (!lua_isnil(current_lua_state, -1)) {
-        has_return_value = true;
-        return_value = lua_tostring(current_lua_state, -1);
+    // Copy results back
+    l_top += MAX_CC_RETURN;
+    lua_checkstack(L, l_top);
+    for (int i = -MAX_CC_RETURN; i < 0; ++i) {
+        copy_value(L, current_lua_state, i);
     }
 
-    // Careful stack handling in case current_lua_state and L are the same
-    lua_settop(current_lua_state, current_top);
-
-    auto return_count = 0;
-    if (has_return_value) {
-        lua_pushstring(L, return_value.c_str());
-        ++return_count;
-    }
-
-    return return_count;
+    lua_settop(current_lua_state, restore_top);
+    return MAX_CC_RETURN;
 }
 
 } // namespace np
